e5a5ab7926aa21fcbfb7b286479e9328231c06a5
marcos echeverria <marcosalbertoecheverria@gmail.com>
2022-07-01 12:39:27 -0300

    maquetado 400px

--- js/index.js
+++ js/index.js
@@ -38,3 +38,9 @@ function scrollActive(){
         }
     })
 } 
+
+window.onscroll = ()=> {
+    const nav = document.getElementById("header")
+    if (this.scrollY >= 200) nav.classList.add("scroll-header");
+    else nav.classList.remove("scroll-header")
+}
